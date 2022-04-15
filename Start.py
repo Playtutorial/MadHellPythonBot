2c90a9d5ed179074a8b11d444a32db46546bb40e
Playtutorial <78595036+Playtutorial@users.noreply.github.com>
2022-04-15 17:39:47 +0300

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,12 @@
+x = input("enter password: ")
+if x == "MadHellBotDiscord":
+    print("correct password!")
+    c = input("enter action (1: password generator; 2: when the cancer hangs on the mountain?) ")
+    if c == "1":
+        print("sorry, that action coming soon...")
+    elif c == "2":
+        print("never.")
+    else:
+        print("invaild action.")
+else:
+    print("incorrect password")
